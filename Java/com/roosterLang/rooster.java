7223930bf197ed5d04da0005eecd5d2f2c837163
Rafayel Gaming <rafayel.amirkhanyan@gmail.com>
2020-07-22 01:55:49 +0000

    file and file naming

--- Java/com/roosterLang/rooster.java
+++ Java/com/roosterLang/rooster.java
@@ -6,9 +6,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
 
-class Main {
-      public static void main(String[] args){
+class rooster {
+  
 
-        
-      }
-  }
+}
